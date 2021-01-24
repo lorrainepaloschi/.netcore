e31eaa33d6e83b8ed7e09c9d8c47159329e24053
Lorraine Vieira <lorraine.paloschi@gmail.com>
2021-01-24 12:17:02 -0300

    instruções - estruturas

---
+++ instruções/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace instruções
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
